a7a6318733a6205593a0e88beec78690cc673879
Ganesh <ganeshpatelchetti@gmail.com>
2025-02-02 12:57:34 +0530

    added server file

--- config/db.js
+++ config/db.js
@@ -1,14 +1,15 @@
 require('dotenv').config();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log('MongoDB connected');
-  } catch (err) {
-    console.error('MongoDB connection error:', err);
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
     process.exit(1);
   }
 };
 
 module.exports = connectDB;
+
